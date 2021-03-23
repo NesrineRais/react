b19cc4ad6ad74ee09e82224e4605a834821778ae
LAPTOP-UIES5OQE\nesrine <rais.nesrine@gmail.com>
2021-03-23 20:57:58 +0100

    formulaire react

--- mon-app/src/App.js
+++ mon-app/src/App.js
@@ -1,19 +1,53 @@
-import logo from './logo.svg';
+import React, { Component } from 'react';
 import './App.css';
 import {PureCompo,PureCompoProps} from './components/PureCompo';
-//importer les 2 composants 
+//importer les 2 composants
+import StateCompo from './components/StateCompo';
+class App extends Component {
+ constructor(props){
+   super(props);
+   this.state = {
+     stateApp : ""
+   }
+ }
+  // onInputChange (val) {
+  //   console.log("val dans app =>"+val);
+  //   this.setState({stateApp:val});
+  //  }
+   onInputChange = (val) =>{
+    console.log("val dans app =>"+val);//ici je récupere le composant qui été dans state
+   //composant val le plus bas l'enfant
+    this.setState({stateApp:val});//on a mis le state app dans le state
+   }
+  render() {
+    let mesLabel = [];
+    mesLabel.push("mike")
+    mesLabel.push("toto")
+    mesLabel.push("titi")
+    mesLabel.push("nesrine")
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-       
-        <PureCompo />
-        <PureCompoProps label={"vue js"} age={"5ans"}/> 
-        
-      </header>
-    </div>
-  );
+    return (
+      <div className="App">
+        <header className="App-header">
+         {mesLabel.map((libelle)=>
+          <StateCompo key={libelle} label={libelle} onInputChange={this.onInputChange}/>
+        )}{/** boucler sur les composant boucle for c'est map*/}
+           {/* <StateCompo label={"toto"} onInputChange={this.onInputChange}/>
+          <StateCompo label={"titi"} onInputChange={this.onInputChange}/>
+          <StateCompo label={"nesrine"} onInputChange={this.onInputChange}/> */}
+
+          <PureCompo />
+          {this.state.stateApp ? <PureCompoProps label={this.state.stateApp} age={"5ans"}/>:<div>Rien n'est remplis</div>}
+          {/** condition if si contenu remplis on fais ca si nn : on fais l autre */}
+
+          {/*//binde est liéer
+          pour récuperer un valeur de la formulaire et on l utiliser dans un autre compo */}
+
+        </header>
+      </div>
+    )
+  };
+
 }
 
 export default App;
---
+++ mon-app/src/components/StateCompo.js
@@ -0,0 +1,42 @@
+/**
+ * @auteur Nesrine
+ * Licence
+ * Premier composant react
+ */
+
+import React,{Component} from 'react';
+
+class StateCompo extends React.Component{
+    constructor(props){
+        super(props);
+        console.log("label " + props.label);
+        this.state = {nom : ''};
+    }
+    // handleChange(e){//le but qund on ecrit dans le but je veux passer dans cette fonction
+    //     console.log("Je passe ici");
+    //     this.setState({nom : "le new"});
+    //     // this.handleChange = this.handleChange.bind(this);
+    // }
+
+    handleChange = (e) => {
+        //e.target.value; //pour récuperer les valeurs de l'input
+        //this.setState({nom : "le new"});//cette ecriture permet de metre par defaut dans l input le text le new
+        console.log("value dans campo =>" +e.target.value);
+        this.setState({nom : e.target.value});
+        this.props.onInputChange(e.target.value);
+    }
+    //on utilison les fonction fléché on est pas besoin de binder le this
+    render(){
+        return(
+            <div>
+                <h6 className="state-h6">Compo state {this.props.label} </h6>
+                {/* //pour appelr un props on fais this.props.labek*/}
+                <input type="text" value = {this.state.nom} onChange={this.handleChange}></input>
+                {/** on veut dans l'input quand on met le curseur dans le chapms et on ecrit il change le contenu
+                
+                il faut binder le this pour mettre le nouvelle valeur du nom*/}
+            </div>
+        );
+    }
+}
+export default StateCompo;
